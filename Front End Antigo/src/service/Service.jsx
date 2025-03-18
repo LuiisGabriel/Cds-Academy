e3ffb4369dcded54bb748a02125d996ac231a965
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-03-18 17:12:55 -0300

    Delete Front End Antigo directory

--- Front End Antigo/src/service/Service.jsx
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://eu-west-2.cdn.hygraph.com/content/cm71z3a6k00lx07usssoh7nfe/master",
-  cache: new InMemoryCache(),
-});
-
-export default client;
