37ee5ec8eb14f6a1c59fd153887502ae718f8fe4
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-03-25 17:54:45 -0300

    Add files via upload

--- Cds-Academy-Back-End/services/authService.js
+++ Cds-Academy-Back-End/services/authService.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import gqlClient from "../graphql/client.js";
-import {CreateNextUserMutation,GetUserByEmailQuery,CreateNextAdminUserMutation,CreateVideoMutation,GetAdminUserByEmailQuery} from "../graphql/mutations.js";
+import { CreateNextUserMutation, GetUserByEmailQuery, CreateNextAdminUserMutation, CreateVideoMutation, GetAdminUserByEmailQuery } from "../graphql/mutations.js";
 
 
 const { JWT_SECRET, JWT_EXPIRES_IN } = process.env;
@@ -29,118 +29,118 @@ class AuthService {
   }
 
 
-async signin(email, password) {
-  const getUserResponse = await gqlClient.request(GetUserByEmailQuery, {
-    email,
-  });
-  const { nextUser } = getUserResponse;
-  if (!nextUser) {
-    throw new Error("Invalid Email Or Password");
+  async signin(email, password) {
+    const getUserResponse = await gqlClient.request(GetUserByEmailQuery, {
+      email,
+    });
+    const { nextUser } = getUserResponse;
+    if (!nextUser) {
+      throw new Error("Invalid Email Or Password");
+    }
+    const isMatch = await bcrypt.compare(password, nextUser.password);
+    if (!isMatch) {
+      throw new Error("Invalid Email Or Password");
+    }
+    const token = jwt.sign(
+      {
+        id: nextUser.id,
+        email: nextUser.email,
+      },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN }
+    );
+    return token;
   }
-  const isMatch = await bcrypt.compare(password, nextUser.password);
-  if (!isMatch) {
-    throw new Error("Invalid Email Or Password");
-  }
-  const token = jwt.sign(
-    {
-      id: nextUser.id,
-      email: nextUser.email,
-    },
-    JWT_SECRET,
-    { expiresIn: JWT_EXPIRES_IN }
-  );
-  return token;
-}
 
 
-async getCurrentUser(token) {
-  const decoded = jwt.verify(token, JWT_SECRET);
-  const getUserResponse = await gqlClient.request(GetUserByEmailQuery, {
-    email: decoded.email,
-  });
-  const { nextUser } = getUserResponse;
-  if (!nextUser) {
-    throw new Error("User not found");
+  async getCurrentUser(token) {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const getUserResponse = await gqlClient.request(GetUserByEmailQuery, {
+      email: decoded.email,
+    });
+    const { nextUser } = getUserResponse;
+    if (!nextUser) {
+      throw new Error("User not found");
+    }
+    delete nextUser.password;
+    return nextUser;
   }
-  delete nextUser.password;
-  return nextUser;  
-}
 
-async adminSignup(adminSignupRequest) {
-  const { email, password, firstname, lastname } = adminSignupRequest;
-  const hashedPassword = await bcrypt.hash(password, 8);
-  const adminUserData = {
-    email,
-    password: hashedPassword,
-    firstname,
-    lastname,
-  };
-  const response = await gqlClient.request(CreateNextAdminUserMutation, {
-    adminUserData,
-  });
-  if (!response?.createNextAdminUser) {
-    throw new Error("CreateNextAdminUser Failed");
+  async adminSignup(adminSignupRequest) {
+    const { email, password, firstname, lastname } = adminSignupRequest;
+    const hashedPassword = await bcrypt.hash(password, 8);
+    const adminUserData = {
+      email,
+      password: hashedPassword,
+      firstname,
+      lastname,
+    };
+    const response = await gqlClient.request(CreateNextAdminUserMutation, {
+      adminUserData,
+    });
+    if (!response?.createNextAdminUser) {
+      throw new Error("CreateNextAdminUser Failed");
+    }
+    const token = jwt.sign({ user: response.createNextAdminUser }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+    return { user: response.createNextAdminUser, token };
   }
-  const token = jwt.sign({ user: response.createNextAdminUser }, JWT_SECRET, {
-    expiresIn: JWT_EXPIRES_IN,
-  });
-  return { user: response.createNextAdminUser, token };
-}
 
-async createVideo(createVideoRequest) {
-  const { titulo, ambiente, modulo, url, subModulo } = createVideoRequest;
-  const videoData = {
-    titulo,
-    ambiente,
-    modulo,
-    url,
-    subModulo,
-  };
-  const response = await gqlClient.request(CreateVideoMutation, {
-    videoData,
-  });
-  if (!response?.createVideo) {
-    throw new Error("CreateVideo Failed");
+  async createVideo(createVideoRequest) {
+    const { titulo, ambiente, modulo, url, subModulo } = createVideoRequest;
+    const videoData = {
+      titulo,
+      ambiente,
+      modulo,
+      url,
+      subModulo,
+    };
+    const response = await gqlClient.request(CreateVideoMutation, {
+      videoData,
+    });
+    if (!response?.createVideo) {
+      throw new Error("CreateVideo Failed");
+    }
+    return { user: response.createVideo };
   }
-  return { user: response.createVideo };
-}
 
 
-async adminSignin(email, password) {
-  const getUserResponse = await gqlClient.request(GetAdminUserByEmailQuery, {
-    email,
-  });
-  const { nextAdminUser } = getUserResponse;
-  if (!nextAdminUser) {
-    throw new Error("Invalid Email Or Password");
+  async adminSignin(email, password) {
+    const getUserResponse = await gqlClient.request(GetAdminUserByEmailQuery, {
+      email,
+    });
+    const { nextAdminUser } = getUserResponse;
+    if (!nextAdminUser) {
+      throw new Error("Invalid Email Or Password");
+    }
+    const isMatch = await bcrypt.compare(password, nextAdminUser.password);
+    if (!isMatch) {
+      throw new Error("Invalid Email Or Password");
+    }
+    const token = jwt.sign(
+      {
+        id: nextAdminUser.id,
+        email: nextAdminUser.email,
+      },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN }
+    );
+    return token;
   }
-  const isMatch = await bcrypt.compare(password, nextAdminUser.password);
-  if (!isMatch) {
-    throw new Error("Invalid Email Or Password");
-  }
-  const token = jwt.sign(
-    {
-      id: nextAdminUser.id,
-      email: nextAdminUser.email,
-    },
-    JWT_SECRET,
-    { expiresIn: JWT_EXPIRES_IN }
-  );
-  return token;
-}
 
-async getCurrentAdminUser(token) {
-  const decoded = jwt.verify(token, JWT_SECRET);
-  const getUserResponse = await gqlClient.request(GetAdminUserByEmailQuery, {
-    email: decoded.email,
-  });
-  const { nextAdminUser } = getUserResponse;
-  if (!nextAdminUser) {
-    throw new Error("User not found");
+  async getCurrentAdminUser(token) {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const getUserResponse = await gqlClient.request(GetAdminUserByEmailQuery, {
+      email: decoded.email,
+    });
+    const { nextAdminUser } = getUserResponse;
+    if (!nextAdminUser) {
+      throw new Error("User not found");
+    }
+    delete nextAdminUser.password;
+    return nextAdminUser;
   }
-  delete nextAdminUser.password;
-  return nextAdminUser;
-}
 
 }
 
